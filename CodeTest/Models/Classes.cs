35cd9b58d596868cc7445a67d3a2b2830077b1a5
theone056 <32253665+theone056@users.noreply.github.com>
2022-06-07 20:58:04 +0800

    added class Classes

---
+++ CodeTest/Models/Classes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTest.Models
+{
+    public class Classes
+    {
+        [Key]
+        [Column(TypeName = "varchar(50)")]
+        public string ClassName { get; set; }
+        [Column(TypeName = "varchar(100)")]
+        public string Location { get; set; }
+        [Column(TypeName ="varchar(50)")]
+        public string TeacherName { get; set; }
+    }
+}
