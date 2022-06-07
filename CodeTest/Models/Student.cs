c8592b74c61a809331e79fcdd4c5973efbd3071b
theone056 <32253665+theone056@users.noreply.github.com>
2022-06-07 20:58:42 +0800

    add class student

---
+++ CodeTest/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeTest.Models
+{
+    public class Student
+    {
+        [Column(TypeName = "varchar(50)")]
+        public string FirstName { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public float GPA { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string ClassName { get; set; }
+    }
+}
