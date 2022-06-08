c314410c30ec952909bb42a6e5aae459066e5dae
theone056 <32253665+theone056@users.noreply.github.com>
2022-06-08 08:40:24 +0800

    add a unittest

--- CodeTest.Test/UnitTest1.cs
+++ CodeTest.Test/UnitTest1.cs
@@ -1,13 +1,65 @@
+using CodeTest.Models;
+using CodeTest.Repository;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace CodeTest.Test
 {
     public class Tests
     {
-        [Test]
-        public void Test1()
-        {
 
+        [Test]
+        public void AddStudentClassNotAvailable()
+        {
+            IRepo repo = new Repo();
+            Student student = new Student()
+            {
+                FirstName = "Derick",
+                LastName = "Colon",
+                Age = 25,
+                GPA = 3.5,
+                ClassName = "Science"
+            };
+
+            var result = Assert.ThrowsAsync<Exception>(()=> repo.AddStudent(student));
+            Assert.That(result.Message, Is.EqualTo("Class not available"));
+        }
+        [Test]
+        public async Task GetClass()
+        {
+            IRepo repo = new Repo();
+            var list = await repo.GetClasses();
+
+            Assert.AreEqual(1, list.Count);
+        }
+        [Test]
+        public async Task GetStudent()
+        {
+            IRepo repo = new Repo();
+            var list = await repo.GetStudents("Biology");
+            Assert.AreEqual(1, list.Count);
+        }
+        [Test]
+        public async Task AddStudent()
+        {
+            IRepo repo = new Repo();
+            Student student = new Student()
+            {
+                FirstName = "Derick",
+                LastName = "Colon",
+                Age = 25,
+                GPA = 3.5,
+                ClassName = "Biology"
+            };
+
+            var result = Assert.ThrowsAsync<Exception>(()=>repo.AddStudent(student));
+            Assert.That(result.Message, Is.EqualTo("Duplicate Student"));
+        }
+        //[Test]
+        public void AddDuplicate()
+        {
+            IRepo repo = new Repo();
             Classes classes = new Classes()
             {
                 ClassName = "Biology",
@@ -15,23 +67,27 @@ namespace CodeTest.Test
                 TeacherName = "Derick"
             };
 
-            Assert.AreEqual(classes, classes);
+            var result = Assert.ThrowsAsync<Exception>(() => repo.AddClass(classes));
+            Assert.That(result.Message, Is.EqualTo("Duplicate Class"));
         }
-    }
 
 
-    public class Classes
-    {
-        public string ClassName { get; set; }
-        public string Location { get; set; }
-        public string TeacherName { get; set; }
+      //  [Test]
+        public async Task AddClass()
+        {
+            IRepo repo = new Repo();
+            Classes classes = new Classes()
+            {
+                ClassName = "Biology",
+                Location = "Room 1",
+                TeacherName = "Derick"
+            };
+
+            var result = await repo.AddClass(classes);
+            Assert.AreEqual(1,result);
+        }
+
+  
     }
 
-    public class Students
-    {
-        public string StudentName { get; set; }
-        public int Age { get; set; }
-        public float GPA { get; set; }
-        public string ClassName { get; set; }
-    }
 }
