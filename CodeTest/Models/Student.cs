e61c5be5d004f17069b6cc52b94526d0f3f8e9a7
theone056 <32253665+theone056@users.noreply.github.com>
2022-06-08 08:39:45 +0800

    add key studentid

--- CodeTest/Models/Student.cs
+++ CodeTest/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,13 +9,15 @@ namespace CodeTest.Models
 {
     public class Student
     {
+        [Key]
+        public int StudentId { get; set; }
         [Column(TypeName = "varchar(50)")]
         public string FirstName { get; set; }
         [Column(TypeName = "varchar(50)")]
         public string LastName { get; set; }
         public int Age { get; set; }
-        public float GPA { get; set; }
-        [Column(TypeName = "varchar(50)")]
+        public double GPA { get; set; }
+        [ForeignKey("ClassName"),Column(TypeName = "varchar(100)")]
         public string ClassName { get; set; }
     }
 }
